18da60169a6c21fe8604edfc9f0cb79e743c2179
im24a-gesztelyif <gesztelyif@bzz.ch>
2025-05-13 11:52:48 +0200

    123

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, send_from_directory
-import os
+from app import create_app
 
-app = Flask(__name__, static_folder='frontend', static_url_path='')
+app = create_app()
 
+# Serve the index.html from the frontend folder
 @app.route('/')
-def serve_index():
-    return send_from_directory(app.static_folder, 'index.html')
+def serve_frontend():
+    return send_from_directory('frontend', 'index.html')
+
+# Serve other static files (like CSS/JS) from the frontend folder
+@app.route('/<path:filename>')
+def serve_static_files(filename):
+    return send_from_directory('frontend', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
