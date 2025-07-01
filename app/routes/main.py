587e599f359fd3ea1fefe2c6259e5825f5f20586
im24a-gesztelyif <gesztelyif@bzz.ch>
2025-07-01 10:54:46 +0200

    ID's dropdown - working - 01/07

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, redirect
+from app.db import get_connection
 
 app = Blueprint('main', __name__)
 
@@ -6,4 +7,22 @@ app = Blueprint('main', __name__)
 def index():
     if 'user_id' not in session:
         return redirect('/')
-    return render_template('index.html', username=session['username'])
+    
+    con = get_connection()
+    cursor = con.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM Kategorie WHERE IstAktiv = 1')
+    kategorien = cursor.fetchall()
+
+    cursor.execute('SELECT * FROM Prioritaet')
+    prioritaeten = cursor.fetchall()
+
+    cursor.execute('SELECT * FROM Fortschritt')
+    fortschritte = cursor.fetchall()
+
+    return render_template('index.html',
+        kategorien=kategorien,
+        prioritaeten=prioritaeten,
+        fortschritte=fortschritte,
+        username=session['username']
+    )
